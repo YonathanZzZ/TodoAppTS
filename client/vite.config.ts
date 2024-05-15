4cc94f72a3ee797bd5d5ef6ab4b7485d6ad97801
Yonathan Zarkovian <yony120@gmail.com>
2024-05-15 11:41:13 +0300

    changed config of output folder

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -6,7 +6,7 @@ const serverURL = 'http://localhost:8080';
 export default defineConfig(() => {
     return {
         build: {
-            outDir: 'build',
+            outDir: 'public',
         },
         plugins: [react()],
     };
