54f7317da1d60ecaae649b22f6f815db4619ac85
Yonathan Zarkovian <yony120@gmail.com>
2024-05-15 12:00:55 +0300

    modified server

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,20 +2,17 @@ import dotenv from 'dotenv';
 dotenv.config(); //load environment variables defined in .env file
 import express from 'express';
 const app = express();
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT;
 import cookieParser from 'cookie-parser';
 import http from 'http';
-import path from 'path';
 import {initializeSocket} from './socketHandler';
 const httpServer = http.createServer(app);
 import cors from 'cors';
 import taskRouter from "./routes/taskRouter";
 import userRouter from "./routes/userRouter";
-const BUILD_PATH = "../client/build/";
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, BUILD_PATH)));
 
 if(!process.env.NODE_ENV || process.env.NODE_ENV === 'development'){
     console.log('server running in development environment');
@@ -28,14 +25,9 @@ if(!process.env.NODE_ENV || process.env.NODE_ENV === 'development'){
 app.use('/api/tasks', taskRouter);
 app.use('/users', userRouter);
 
-app.use("/", (req, res) => {
+app.use("/", (_req, res) => {
     res.send("Server is running");
 })
-
-// app.get("*", (_req, res) => {
-//     // res.sendFile(path.join(__dirname, BUILD_PATH + "index.html"));
-//     res.send("Server is running");
-// })
 
 initializeSocket(httpServer);
 
