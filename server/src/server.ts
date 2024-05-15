7d55cae29ed41c77c75484f0c8796a9dd4b3bb67
Yonathan Zarkovian <yony120@gmail.com>
2024-05-15 13:50:04 +0300

    temporarily disabled non-root server routes, for deployment testing

--- server/src/server.ts
+++ server/src/server.ts
@@ -19,8 +19,8 @@ app.use(cors({
     credentials: true,
 }));
 
-app.use('/api/tasks', taskRouter);
-app.use('/users', userRouter);
+// app.use('/api/tasks', taskRouter);
+// app.use('/users', userRouter);
 
 app.get("/", (_req, res) => {
     res.send("Server is running");
